a34c8a9928700be312bc455665bbf8da47b46465
flygsoty <kentashimomura69@gmail.com>
2025-09-30 22:22:43 +0900

    Create env.js

---
+++ public/assets/js/env.js
@@ -0,0 +1,8 @@
+// /public/assets/js/env.js
+// 環境変数をブラウザで利用できるように定義
+// 本番では Cloudflare Pages の環境変数 → ビルド時に書き換える運用がおすすめ
+
+window._env_ = {
+  SUPABASE_URL: "https://ffzxepgcfylighukicsq.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmenhlcGdjZnlsaWdodWtpY3NxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNzk3MjYsImV4cCI6MjA3NDc1NTcyNn0.mJO9MQ4vQbEdY6bygSEF9pWT9BU2uZsmSI_V0RVsQeg"
+};
