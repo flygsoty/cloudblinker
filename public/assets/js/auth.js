2729ff4fb6939c7b04fdf3c71ba4fef84ccc7859
flygsoty <kentashimomura69@gmail.com>
2025-09-30 22:25:56 +0900

    Update auth.js

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -63,7 +63,7 @@ if (googleBtn) {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin + "/auth/callback"
+        redirectTo: window.location.origin + "/client/index.html"
       }
     });
 
